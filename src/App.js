d7e679c3bfe331bb7e63537279e20ddb35d3f5bb
Dev-KoushikDas <das.koushik2609@gmail.com>
2023-08-13 12:22:05 +0530

    ar-commit-1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage';
 import ProductPage from './pages/ProductPage/ProductPage';
-
+import Ar from './pages/Ar/Ar';
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +10,7 @@ function App() {
         <Route exact path='/' element={<div>hello</div>} />
         <Route path='/home' element={<HomePage/>} />
         <Route path='/product' element={<ProductPage/>} />
+         <Route path='/ar' element={<Ar/>} />
       </Routes>
     </BrowserRouter>
   );
